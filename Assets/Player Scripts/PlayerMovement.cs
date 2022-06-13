b2e849e6c2f774d7ea2bcf4874bf32397634ff50
Audrius Petrauskas <audrius.j.petrauskas@gmail.com>
2022-06-14 00:22:11 +0100

    Add player running and idle animations

--- Assets/Player Scripts/PlayerMovement.cs
+++ Assets/Player Scripts/PlayerMovement.cs
@@ -9,12 +9,16 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody2D rb;
 
+    public Animator animator;
+
     Vector2 movement;
 
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Speed", Mathf.Abs(movement.x));
     }
 
     void FixedUpdate()
